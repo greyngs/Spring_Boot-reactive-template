dd762687d7b4840862365161e3061e5a58e5b861
Jorge Reyes <reyesjorge0116@gmail.com>
2022-07-01 23:33:27 -0500

    first version

---
+++ src/main/java/com/jorgereyes/reactive/dto/ProductDto.java
@@ -0,0 +1,15 @@
+package com.jorgereyes.reactive.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductDto {
+    private String id;
+    private String name;
+    private Integer qty;
+    private double price;
+}
